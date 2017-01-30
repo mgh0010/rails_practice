90440569a09cce9995c419099a62c680d815895d
Michaelguyson <mghharrison@gmail.com>
2017-01-30 22:22:28 +0000

    lecture 68 done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+    
   def new
       @article = Article.new
   end
@@ -7,19 +8,35 @@ class ArticlesController < ApplicationController
       @article = Article.new(article_params)
       if @article.save
           flash[:notice] = "Article was successfully created"
-          render 'show'
+          redirect_to article_path(@article)
+          #render 'show'
       else
           render 'new'
       end
   end
   
   def index
-      
+      @articles = Article.all
   end
   
   def show
       @article = Article.find(params[:id]) 
   end
+  
+  def edit
+      @article = Article.find(params[:id])
+  end
+  
+  def update
+      @article = Article.find(params[:id])    
+      if @article.update(article_params)
+          flash[:notice] = "Article updated successfully"
+          redirect_to article_path(@article)
+      else
+          render :edit
+      end
+  end
+     
   
   private
   def article_params
